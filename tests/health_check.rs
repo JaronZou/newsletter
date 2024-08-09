de1e0454751d6e7065c964418cd18b126e63f8a1
JaronZou <JaronZou@outlook.com>
2024-08-09 12:14:39 +0800

    format source code

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,10 +5,10 @@ async fn health_check_works() {
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application
     let client = reqwest::Client::new();
-    // The really interesting things happened here, the .await here 
+    // The really interesting things happened here, the .await here
     // caused the `tokio` runtime to asynchronized to poll both client's
-    // send event and the server's running event, but we only wait the 
-    // `send` to finish. Once the `send` is completed, we will get a 
+    // send event and the server's running event, but we only wait the
+    // `send` to finish. Once the `send` is completed, we will get a
     // response from the server, and we do all kinds of checks. Whether
     // the checks are passed or failed, the test is over, it will close
     // along with the *server* running behind it.
@@ -23,13 +23,12 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
- // No .await call, therefore no need for `spawn_app` to be asyn cnow.
- // We are also running tests, so it is not worth it to propagate errors:
- // if we fail to perform the required setup we can just panic and crash
- // all the things.
+// No .await call, therefore no need for `spawn_app` to be asyn cnow.
+// We are also running tests, so it is not worth it to propagate errors:
+// if we fail to perform the required setup we can just panic and crash
+// all the things.
 fn spawn_app() -> String {
-    let listener = std::net::TcpListener::bind("localhost:0")
-        .expect("Failed to bind random port");
+    let listener = std::net::TcpListener::bind("localhost:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let server = newsletter::run(listener).expect("Failed to bind address");
     // Launch the server as a background task
