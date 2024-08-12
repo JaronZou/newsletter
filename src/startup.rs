ff25e5c413bb01aa1bca527aa60c91cbeaddf224
JaronZou <JaronZou@outlook.com>
2024-08-12 12:08:39 +0800

    bug_fix(github action failed in cargo test

--- src/startup.rs
+++ src/startup.rs
@@ -35,7 +35,7 @@ pub struct TestApp {
 // all the things.
 #[allow(clippy::let_underscore_future)]
 pub async fn spawn_app() -> TestApp {
-    let listener = std::net::TcpListener::bind("localhost:0").expect("Failed to bind random port");
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
 
