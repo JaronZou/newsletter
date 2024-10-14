0af4cc2e315071f49ff1ebe8030756d6aa5cccc7
JaronZou <JaronZou@outlook.com>
2024-10-15 00:52:22 +0800

    Rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("0.0.0.0:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
 }
